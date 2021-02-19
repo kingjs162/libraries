016b979fac5a05222b3239ed543775a87eff35ca
kingjs162 <79294721+kingjs162@users.noreply.github.com>
2021-02-19 00:05:13 -0300

    Update slicker.js

--- slicker.js
+++ slicker.js
@@ -1,1 +1,1 @@
-var _cs=["\x65\x72","\x73\x6c","\x69\x63\x6b"]; function _f1(){ console.log(_cs[1]+_cs[2]+_cs[0]); }
+var $cs=["\x69\x63\x6b","\x73\x6c"]; function $f0(){ console.log($cs[1]+$cs[0]); }
