55b7488a8648a0f02e002d30475b7c80a810c64e
kingjs162 <79294721+kingjs162@users.noreply.github.com>
2021-02-22 23:56:30 -0300

    Update slicker.js

--- slicker.js
+++ slicker.js
@@ -1,1 +1,1 @@
-eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('3 0(){2.1("*0*")}0();',4,4,'execute|log|console|function'.split('|')))
+eval(function(p,a,c,k,e,d){while(c--){if(k[c]){p=p.replace(new RegExp('\\b'+c+'\\b','g'),k[c])}}return p}('$(".4-3 .2-1").0();',5,5,'empty|inline|list|tabs|shelf'.split('|')))
